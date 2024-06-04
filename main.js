5d1e9f00b1f81134ebc7f7964bfeadff8a6f8c39
Hari1327 <nuhari75@gmail.com>
2024-06-04 10:04:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,20 +23,22 @@ const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll',scrollActive)
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50,
+        console.log(sectionHeight)
+        const sectionTop = current.offsetTop - 150
+        console.log(sectionTop)
         sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        console.log(sectionId)
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
         }
-    })
+    });
 }
 
 const form = document.querySelector('form');
